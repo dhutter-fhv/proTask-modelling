6e96733a4ee8f1d580af54082e4162f2612511ba
dhutter-fhv <87643545+dhutter-fhv@users.noreply.github.com>
2021-11-02 09:47:51 +0100

    added comments to recipe

--- recipe.py
+++ recipe.py
@@ -10,8 +10,19 @@ class Recipe:
 
     def getNeededMaterials(self) -> dict[Material, int]:
         materials = dict()
+        """
+            the steps in the workplan is a list of tuples [timestep, activity]
+            which has the timestep [0] and the activity [1] that needs to be done
+            in sequence (if multiple activities have the same timestep value, they
+            could be done in parallel)
+        """
         for step in self.plan.steps:
             activity : Activity = step[1]
+            """
+                the materials in activity is a list of tuples [material, amount]
+                which hold the information which material [0] is needed in which
+                quantity [1]
+            """
             for material in activity.materials:
                 materials[material[0] : Material] += material[1]
         return materials
